d94d94c190356d1da4b4f75e20bc1aefb2259ccd
ShiraOzeri <shirahalevy2@gmail.com>
2019-08-09 02:18:50 +0300

    hole filling

--- HoleFilling/HoleFillingRunnable.java
+++ HoleFilling/HoleFillingRunnable.java
@@ -31,11 +31,11 @@ public class HoleFillingRunnable {
 		validSize(image,mask);
 		
 		Picture picture=new Picture(image);
-//		HoleFilling h_f=new HoleFilling(picture, mask, connect,z,e);
-//		Picture fill_picture=h_f.holeFillingAlgo();
-		
-		ApproximatesHoleFilling app_hf=new ApproximatesHoleFilling(picture, mask, connect,z,e);
-		Picture fill_picture=app_hf.holeFillingAlgo();
+		HoleFilling h_f=new HoleFilling(picture, mask, connect,z,e);
+		Picture fill_picture=h_f.holeFillingAlgo();
+//		
+//		ApproximatesHoleFilling app_hf=new ApproximatesHoleFilling(picture, mask, connect,z,e);
+//		Picture fill_picture=app_hf.holeFillingAlgo();
 		
 		String path=path1.substring(0,path1.indexOf("."))+"FILL"+path1.substring(path1.indexOf("."));
 
